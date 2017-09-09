89d48fc7002a89267915a76e44d37ccc53fea22a
Jacky Tang <jackytck@gmail.com>
2017-09-10 00:30:13 +0800

    :construction: post to create user + request one time password

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -7,15 +7,33 @@ import {
   FormInput,
   Button
 } from 'react-native-elements'
+import axios from 'axios'
+
+const ROOT_URL = 'https://us-central1-one-time-password-9cc06.cloudfunctions.net'
 
 class SignUpForm extends Component {
+  state = { phone: '' }
+
+  handleSubmit = async () => {
+    const { phone } = this.state
+    try {
+      await axios.post(`${ROOT_URL}/createUser`, { phone })
+      await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   render () {
     return (
       <View>
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
-          <FormInput />
-          <Button title='Submit' />
+          <FormInput
+            value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}
+          />
+          <Button title='Submit' onPress={this.handleSubmit} />
         </View>
       </View>
     )
